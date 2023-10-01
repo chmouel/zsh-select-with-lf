d8aee24340fb64c8c75445d07fc4867219fa3857
Chmouel Boudjnah <chmouel@chmouel.com>
2023-10-01 07:00:07 +0200

    Simplify the plugin
    no need to use a temporary file, lf can print the selection to stdout
    for file as well as for dir

--- zsh-select-with-lf.plugin.zsh
+++ zsh-select-with-lf.plugin.zsh
@@ -4,28 +4,16 @@
 (( ! ${+ZSH_SELECT_WITH_LF_DIR_BIND} )) && typeset -g ZSH_SELECT_WITH_LF_DIR_BIND='^xl'
 
 __select_with_lf_file() {
-    action=${1:-file}
+    action=${1:-print-selection}
     local current=${LBUFFER##* }
-    [[ $action == file ]] && {
-        local tmpfile=${TMPPREFIX:-/tmp/.zsh}-zsl-$$
-        command lf -selection-path=$tmpfile
-        ret=$?
-        [[ ! -e $tmpfile ]] && return 1
-        selection=$(cat "$tmpfile")
-        rm -f $tmpfile
-    }
-    [[ $action == dir ]] && {
-        selection=$(command lf -print-last-dir )
-        ret=$?
-    }
+    selection="$(command lf -${action} )"
+    [[ -z ${selection} ]] && return 1
     [[ -n ${current} ]] && LBUFFER="${LBUFFER% *} "
     LBUFFER="$LBUFFER$selection"
-    return $ret
 }
 
 __select_with_lf_dir() {
-    __select_with_lf_file dir
-    return $?
+    __select_with_lf_file print-last-dir
 }
 
 autoload -U __select_with_lf_file __select_with_lf_dir
